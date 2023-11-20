2cb0577ae5db37ae5bbe646c5b3d0285ff732e11
Aiden Peace <aidenpeace300@gmail.com>
2023-11-20 19:41:09 +0000

    Completed navgation and protfolio introduction with links to my linkedIn and Github

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,83 @@
-import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import logo from '../images/aidensLogo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // Import the styles
 
 export default function App() {
-  const [serverData, setServerData] = useState('');
-
-  useEffect(() => {
-    async function readServerData() {
-      const resp = await fetch('/api/hello');
-      const data = await resp.json();
-
-      console.log('Data from server:', data);
-
-      setServerData(data.message);
-    }
-
-    readServerData();
-  }, []);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div>
+      <header>
+        <img className="logo" src={logo} alt="logo" />
+        <nav>
+          <ul className="nav__links">
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#services">Services</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+          </ul>
+        </nav>
+        <a className="cta" href="#contact">
+          <button>Contact</button>
         </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>{serverData}</h1>
-    </>
+      </header>
+      <section id="home" className="meet">
+        <div className="container">
+          <div className="content">
+            <div className="meet-main">
+              <div className="meet-text">
+                <h1>
+                  Front-End React <br /> Developer
+                </h1>
+                <p>
+                  Hi, I'm Aiden Peace. A passionate Front-end React Developer
+                  based in Lake Forest, California. 📍
+                </p>
+                <span>
+                  <a
+                    href="https://www.linkedin.com/in/aiden-peace-025918211/"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <FontAwesomeIcon
+                      icon={faLinkedin}
+                      className="fab fa-linkedin"
+                    />
+                  </a>
+                  <a
+                    href="https://github.com/Aiden-Peace300"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <FontAwesomeIcon
+                      icon={faGithub}
+                      className="fab fa-github"
+                    />
+                  </a>
+                </span>
+              </div>
+              <div className="photoOfAiden"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="services" className="services">
+        <div className="container">
+          <div className="skill-content">
+            <h2>Services</h2>
+            <div className="skills">
+              <p>Tech Stack Skills</p>
+              <div className="logos"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
